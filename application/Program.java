a215adad855bd44945484edc268dbe665519c978
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-02-07 14:04:05 -0300

    Update

--- application/Program.java
+++ application/Program.java
@@ -2,9 +2,9 @@ import boardgame.Position;
 
 public class Program
 {
-    public static void main(String[] args)
-    {
+	public static void main(String[] args)
+	{
 		Position p = new Position(3, 2);
 		System.out.println(p);
-    }
+	}
 }
